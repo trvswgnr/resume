8c362ee7e114cc644b44a2749d70e592fe48d358
trav <trvswgnr@gmail.com>
2018-09-27 02:24:29 -0400

    add get the skills from the database

--- modules/skills.php
+++ modules/skills.php
@@ -1,12 +1,17 @@
-<?php include '../data/skills'; ?>
+<?php
+$skills = $conn->query("SELECT skill FROM skills");
+$skills = $skills->fetchAll();
+?>
 <div class="skills">
 	<div class="row">
 		<h3>Skills</h3>
+		<div class="skills-list">
 		<?php
 		foreach ($skills as $skill) {
-			echo $skill;
+			echo "<div class='skill'>$skill[0]</div>";
 		}
 		?>
+		</div>
 	</div>
 </div>
 <hr/>
