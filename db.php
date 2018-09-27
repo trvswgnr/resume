6ee75e198f724e0a2425b14c182c88a85938c33d
trav <trvswgnr@gmail.com>
2018-09-27 00:51:12 -0400

    successful connection

--- db.php
+++ db.php
@@ -4,10 +4,7 @@ $db = "travisaw_personal";
 $user = "travisaw_taw";
 $pass = "Door1Stop2!";
 
-$conn = new PDO("mysql:host=$host;", $username, $password, $db);
+/* make the database connection */
+$conn = new PDO("mysql:host=$host;", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_USE_BUFFERED_QUERY);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
+if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
