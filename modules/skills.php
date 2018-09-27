69f7ec08f7783c459c323af414951ae07f40d9cb
trav <trvswgnr@gmail.com>
2018-09-27 00:39:21 -0400

    add skills

---
+++ modules/skills.php
@@ -0,0 +1,12 @@
+<?php include '../data/skills'; ?>
+<div class="skills">
+	<div class="row">
+		<h3>Skills</h3>
+		<?php
+		foreach ($skills as $skill) {
+			echo $skill;
+		}
+		?>
+	</div>
+</div>
+<hr/>
