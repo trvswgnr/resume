dcb2068a128d347364683a7c60301368e08da1e7
trav <trvswgnr@gmail.com>
2018-09-27 02:23:38 -0400

    formatting

--- db.php
+++ db.php
@@ -1,10 +1,13 @@
 <?php
-$host = "localhost";
-$db = "travisaw_personal";
-$user = "travisaw_taw";
-$pass = "Door1Stop2!";
+$host  = "localhost";
+$db    = "travisaw_personal";
+$user  = "travisaw_taw";
+$pass  = "Door1Stop2!";
 
-/* make the database connection */
-$conn = new PDO("mysql:host=$host;", $username, $password);
+// make the database connection
+$conn = new PDO("mysql:host=$host;", $user, $pass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_USE_BUFFERED_QUERY);
 if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+
+// use the database
+$conn->query("USE $db") or die(var_dump($conn->errorInfo()));
