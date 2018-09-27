2445cc6b6b52248f25f8257de26afb0455bc68dd
trav <trvswgnr@gmail.com>
2018-09-27 00:38:21 -0400

    add jobs

---
+++ data/jobs.php
@@ -0,0 +1,32 @@
+<?php
+$jobs = [
+	[
+		"company"     => "Chargebacks911",
+		"year"        => "2018",
+		"location"    => "Clearwater, FL",
+		"position"    => "Front End Developer",
+		"description" => "Worked on a team where I designed and developed a single page web app for on-boarding new clients. Quickly created tailored, responsive landing pages for targeted campaigns with Hubl templating language. Created a streamlined, module-based workflow using current best practices and latest web technologies such as Sass/SCSS, React, Webpack, and Babel. Optimized primary company website for speed and usability. Created Wordpress plugins and components."
+	],
+	[
+		"company"     => "Sparxoo",
+		"year"        => "2017",
+		"location"    => "Tampa, FL",
+		"position"    => "Web & Mobile Developer",
+		"description" => "Constructed workflow and standards for quick production and seamless handoff. Worked directly with designers and accounts to create best-in-class websites for premier clients from a variety of industries."
+	],
+	[
+		"company"     => "Fourth Coast Media",
+		"year"        => "2016",
+		"location"    => "Pensacola, FL",
+		"position"    => "Tech Lead",
+		"description" => "Worked directly with clients and team members to create custom websites tailored to specific situations. Implemented a variety of frameworks and libraries for rapid prototyping and quick development. Set up and maintained multiple ecommerce and database-driven sites on Shopify, Bigcommerce, and WordPress. Optimized sites for search engine visibility and managed SEM accounts."
+	],
+	[
+		"company"     => "Cyber Sytes",
+		"year"        => "2015",
+		"location"    => "Panama City, FL",
+		"position"    => "Developer & Project Manager",
+		"description" => "Organized and web projects from start to finish, working with sales staff and clients directly. Set up Content Management Systems and trained clients to use newly developed features."
+	]
+];
+?>
