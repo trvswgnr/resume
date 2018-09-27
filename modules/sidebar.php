64a84c84cb3f6dbd7d90db18f7c6ebe947d433d0
trav <trvswgnr@gmail.com>
2018-09-27 00:39:09 -0400

    add sidebar

---
+++ modules/sidebar.php
@@ -0,0 +1,32 @@
+<div class="leftbar">
+	<div class="topsec">
+		<div class="profilePic"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/230271/Travis-resized-edit-2.png" alt="Travis Aaron Wagner" /></div>
+	</div>
+	<div class="bottomsec">
+		<div class="name">
+			<h1>Travis A. Wagner</h1>
+		</div>
+		<div class="info">
+			<h2>developer / digital specialist</h2><a class="email" href="#"></a>
+		</div>
+		<hr/>
+		<h3>Objective</h3>
+		<p class="profile">I am looking for a position that utilizes my skills and experience as a developer along with my background in design, marketing, and management to implement best practices and ultimately create faster, more efficient workflows.</p>
+		<hr/>
+		<h3>Contact</h3>
+		<div class="m-row">
+			<div class="col-m-6">
+				<h4>Phone</h4>
+				<p><a href="tel:15743617729">+1 (574) 361-7729</a></p>
+				<h4>Email</h4>
+				<p><a href="mailto:trvswgnr@gmail.com">trvswgnr@gmail.com</a></p>
+			</div>
+			<div class="col-m-6">
+				<h4>GitHub</h4>
+				<p>trvswgnr</p>
+				<h4>Address</h4>
+				<p>2780 20th Ave SW<br/>Largo, FL 33774</p>
+			</div>
+		</div>
+	</div>
+</div>
