a451352f4f9f745b93a5b7723d594d171052d2f6
trav <trvswgnr@gmail.com>
2018-09-27 00:38:49 -0400

    add index

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Travis Wagner | Résumé</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+	<link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
+	<link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Playfair+Display:400,900,900i|Raleway:300,400,700'>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<div class="container resume">
+		<?php include 'modules/sidebar.php'; ?>
+		<div class="main">
+			<?php include 'modules/skills.php'; ?>
+			<?php include 'modules/experience.php'; ?>
+			<div class="softskills">
+				<h3>Soft Skills</h3>
+				<ul class="chips">
+					<li>Research</li>
+					<li>Communication</li>
+					<li>Critical Thinking</li>
+					<li>Leadership</li>
+					<li>Flexibility</li>
+				</ul>
+			</div>
+			<hr/>
+			<div class="education">
+				<h3>Education</h3>
+				<div class="row">
+					<div class="rightCol">
+						<h4>Full Sail University</h4>
+						<h5>B.S. in Music Production <span>•</span> Winter Park, FL</h5>
+					</div>
+					<div class="rightCol2">
+						<div class="schoolLogo"><img src="https://myfullsailjourney.files.wordpress.com/2013/04/fullsail1.png" alt="" /></div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="rightCol">
+						<h4>Ferris State University</h4>
+						<h5>Graphic Design <span>•</span> Big Rapids, MI</h5>
+					</div>
+					<div class="rightCol2">
+						<div class="schoolLogo"><img class="ferrisLogo" src="https://www.ferris.edu/HTMLS/administration/advance/standards/logos/thumbnails/01-BoxFlameFullColor.png" alt="" /></div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="rightCol">
+						<h4>Constantine High School</h4>
+						<h5>H.S. Diploma <span>•</span> Constantine, MI</h5>
+					</div>
+					<div class="rightCol2">
+						<div class="schoolLogo"><img class="ctownLogo" src="http://travisawagner.com/wp-content/uploads/2017/04/ctown-falcons.gif" alt="" /></div>
+					</div>
+				</div>
+			</div>
+			<hr/>
+			<div class="links">
+				<h3>Links</h3>
+				<div class="row">
+					<div class="col-6">
+						<h4><i class="fa fa-lg fa-behance" aria-hidden="true"></i></h4>
+						<h5><a href="https://www.behance.net/traviswagner">https://www.behance.net/traviswagner</a></h5>
+					</div>
+					<div class="col-6">
+						<h4><i class="fa fa-lg fa-twitter" aria-hidden="true"></i></h4>
+						<h5><a href="https://www.twitter.com/trvswgnr">https://www.twitter.com/trvswgnr</a></h5>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-6">
+						<h4><i class="fa fa-lg fa-linkedin" aria-hidden="true"></i></h4>
+						<h5><a href="https://www.linkedin.com/in/travisawagner">https://www.linkedin.com/in/travisawagner</a></h5>
+					</div>
+					<div class="col-6">
+						<h4><i class="fa fa-lg fa-facebook" aria-hidden="true"></i></h4>
+						<h5><a href="https://www.facebook.com/trvswgnr">https://www.facebook.com/trvswgnr</a></h5>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
