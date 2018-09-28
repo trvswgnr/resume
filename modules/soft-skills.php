8e53af4a0374ab52762d514a2dfcaf2081f9ced7
trav <trvswgnr@gmail.com>
2018-09-27 21:48:00 -0400

    add soft-skills.php

---
+++ modules/soft-skills.php
@@ -0,0 +1,11 @@
+<div class="softskills">
+	<h3>Soft Skills</h3>
+	<ul class="chips">
+		<li>Research</li>
+		<li>Communication</li>
+		<li>Critical Thinking</li>
+		<li>Leadership</li>
+		<li>Flexibility</li>
+	</ul>
+</div>
+<hr/>
