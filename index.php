23389677d5b08ffb120710ac1c29d5616825b655
trav <trvswgnr@gmail.com>
2018-09-28 00:09:25 -0400

    use the file modified time as the file version (for cache busting)

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ include 'db.php';
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
 	<link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Playfair+Display:400,900,900i|Raleway:300,400,700'>
-	<link rel="stylesheet" href="dist/style.css">
+	<?php $css_version = filemtime('./dist/style.css'); ?>
+	<link rel="stylesheet" href="dist/style.css?ver=<?php echo $css_version; ?>">
 </head>
 
 <body>
